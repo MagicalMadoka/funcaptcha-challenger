8c1cbd49243878cdfa148e99c6aaa391c50ede68
madoka <s@madokax.com>
2023-11-23 11:42:33 +0800

    hopscotch

--- funcaptcha_challenger/hopscotch_highsec.py
+++ funcaptcha_challenger/hopscotch_highsec.py
@@ -1,19 +1,20 @@
 import numpy as np
 
 from funcaptcha_challenger.model import BaseModel
+from funcaptcha_challenger.predictor import FuncaptchaPredictor
 from funcaptcha_challenger.tools import check_input_image_size, process_image
 
 
-class AnimalRotationPredictor:
+class HopscotchHighsecPredictor(FuncaptchaPredictor):
+
     def __init__(self):
-        self.model = BaseModel("animal_rotation_towards_hand.onnx")
+        super().__init__()
+        self.model = BaseModel("hopscotch_highsec.onnx")
 
-    def _run_prediction(self, left, right):
-        return self.model.run_prediction(None, {'input_left': left.astype(np.float32),
-                                                'input_right': right.astype(np.float32)})[0]
-
-    def predict(self, image) -> int:
+    def _check_input_image_size(self, image):
         check_input_image_size(image)
+
+    def _predict(self, image) -> int:
 
         max_prediction = float('-inf')
         max_index = -1
@@ -31,3 +32,7 @@ class AnimalRotationPredictor:
                 max_index = i
 
         return max_index
+
+    def _run_prediction(self, left, right):
+        return self.model.run_prediction(None, {'input_left': left.astype(np.float32),
+                                                'input_right': right.astype(np.float32)})[0]
--- tests/test_hopscotch_highsec.py
+++ tests/test_hopscotch_highsec.py
@@ -4,23 +4,23 @@ import unittest
 from PIL import Image
 from loguru import logger
 
-from funcaptcha_challenger import predict_numericalmatch
+from funcaptcha_challenger import predict_hopscotch_highsec
 from tests.utils import get_ans_from_image_name
 
 
-class TestMatchCount(unittest.TestCase):
+class TestHopscotchHighsec(unittest.TestCase):
 
     def setUp(self):
         this_dir = os.path.dirname(os.path.realpath(__file__))
-        self.images_path = os.path.join(this_dir, '../demo', 'captcha-images', 'numericalmatch')
+        self.images_path = os.path.join(this_dir, '../demo', 'captcha-images', 'hopscotch_highsec')
 
     def test_predict(self):
         images_name = os.listdir(self.images_path)
 
         for image_name in images_name:
             with self.subTest(image_name=image_name):
-                logger.info(f'test match count image name is {image_name}')
+                logger.info(f'test  hopscotch_highsec image name is {image_name}')
                 image_path = os.path.join(self.images_path, image_name)
                 image = Image.open(image_path)
-                index = predict_numericalmatch(image)
+                index = predict_hopscotch_highsec(image)
                 self.assertEqual(index, get_ans_from_image_name(image_name))
