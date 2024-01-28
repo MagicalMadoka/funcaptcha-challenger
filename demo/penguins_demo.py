f6e2735bad9c01cb30b785d76328a95d6debd16e
Madoka <s@madokax.com>
2024-01-28 20:44:35 +0800

    fix: penguins model name (#14)

--- demo/penguins_demo.py
+++ demo/penguins_demo.py
@@ -3,14 +3,14 @@ import os
 from PIL import Image
 from loguru import logger
 
-from funcaptcha_challenger import predict_penguin
+from funcaptcha_challenger import predict_penguins
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 
-image_path = os.path.join(this_dir, 'captcha-images', 'penguin',
+image_path = os.path.join(this_dir, 'captcha-images', 'penguins',
                           '0b5bea73081263debe1ccb062816800b285355acf3edd57dea115a62c44ec244_1.jpg')
 
 image = Image.open(image_path)
 
-index = predict_penguin(image)
+index = predict_penguins(image)
 logger.info(f"Predicted index: {index}")
