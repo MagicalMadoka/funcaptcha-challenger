f0263c5ac996ec381cb7407d5dc7d098837b2152
Madoka <s@madokax.com>
2024-02-21 23:51:29 +0800

    Hand number puzzle (#20)
    * feat: hand number puzzle support
    
    * feat: rockstack、hand_number_puzzle
    、knotsCrossesCircle support
    
    * feat: rockstack、hand_number_puzzle
    、knotsCrossesCircle support

--- funcaptcha_challenger/__init__.py
+++ funcaptcha_challenger/__init__.py
@@ -3,13 +3,17 @@ import io
 from PIL import Image
 
 from funcaptcha_challenger.BrokenJigsawbrokenjigsaw_swap import BrokenJigsawbrokenjigsawSwapPredictor
+from funcaptcha_challenger.card import CardPredictor
 from funcaptcha_challenger.coordinatesmatch import CoordinatesMatchPredictor
 from funcaptcha_challenger.counting import CountingPredictor
 from funcaptcha_challenger.dicematch import DicematchMatchPredictor
 from funcaptcha_challenger.frankenhead import FrankenheadPredictor
+from funcaptcha_challenger.hand_number_puzzle import HandNumberPuzzlePredictor
 from funcaptcha_challenger.hopscotch_highsec import HopscotchHighsecPredictor
+from funcaptcha_challenger.knotsCrossesCircle import KnotsCrossesCirclePredictor
 from funcaptcha_challenger.numericalmatch import NumericalmatchPredictor
 from funcaptcha_challenger.penguins import PenguinsPredictor
+from funcaptcha_challenger.rockstack import RockstackPredictor
 from funcaptcha_challenger.shadows import ShadowsPredictor
 from funcaptcha_challenger.threed_rollball_animal import ThreeDRollballAnimalPredictor
 from funcaptcha_challenger.threed_rollball_objects import ThreeDRollballObjectsPredictor
@@ -22,11 +26,15 @@ predictors = [
     CoordinatesMatchPredictor(),
     TrainCoordinatesPredictor(),
     DicematchMatchPredictor(),
+    RockstackPredictor(),
     PenguinsPredictor(),
     ShadowsPredictor(),
     FrankenheadPredictor(),
     BrokenJigsawbrokenjigsawSwapPredictor(),
     CountingPredictor(),
+    HandNumberPuzzlePredictor(),
+    KnotsCrossesCirclePredictor(),
+    CardPredictor(),
 ]
 
 
