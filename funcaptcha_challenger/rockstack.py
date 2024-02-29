e51db84ee9027e549b7c5cf54a0ff3a265827ef2
Madoka <s@madokax.com>
2024-02-29 13:55:25 +0800

    feat: gray model (#28)

--- funcaptcha_challenger/rockstack.py
+++ funcaptcha_challenger/rockstack.py
@@ -5,7 +5,10 @@ from funcaptcha_challenger.predictor import ImagePairClassifierPredictor
 class RockstackPredictor(ImagePairClassifierPredictor):
 
     def _get_model(self):
-        return BaseModel("rockstack.onnx")
+        return BaseModel("rockstack_v2.onnx")
 
     def is_support(self, variant, instruction):
         return 'rockstack' == variant
+
+    def image_color_mode(self):
+        return 'gray'
