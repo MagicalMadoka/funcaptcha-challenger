c1d5623184e003da5169363a5b202982173f4901
Madoka <s@madokax.com>
2024-03-16 16:37:14 +0800

    fix: input shape (#33)

--- funcaptcha_challenger/__init__.py
+++ funcaptcha_challenger/__init__.py
@@ -13,7 +13,6 @@ from funcaptcha_challenger.frankenhead import FrankenheadPredictor
 from funcaptcha_challenger.hand_number_puzzle import HandNumberPuzzlePredictor
 from funcaptcha_challenger.hopscotch_highsec import HopscotchHighsecPredictor
 from funcaptcha_challenger.knotsCrossesCircle import KnotsCrossesCirclePredictor
-from funcaptcha_challenger.numericalmatch import NumericalmatchPredictor
 from funcaptcha_challenger.penguins import PenguinsPredictor
 from funcaptcha_challenger.penguins_icon import PenguinsIconPredictor
 from funcaptcha_challenger.rockstack import RockstackPredictor
@@ -54,17 +53,3 @@ def predict_from_bytes(image_bytes, variant, instruction):
     image_stream = io.BytesIO(image_bytes)
     image = Image.open(image_stream)
     return predict(image, variant, instruction)
-
-
-predict_numericalmatch = NumericalmatchPredictor().predict
-
-# will be removed later
-predict_3d_rollball_animals = lambda image: predict(image, '3d_rollball_animals')
-predict_hopscotch_highsec = lambda image: predict(image, 'hopscotch_highsec')
-predict_3d_rollball_objects = lambda image: predict(image, '3d_rollball_objects')
-predict_coordinatesmatch = lambda image: predict(image, 'coordinatesmatch')
-predict_train_coordinates = lambda image: predict(image, 'train_coordinates')
-predict_dicematch = lambda image: predict(image, 'dicematch')
-
-predict_penguins = lambda image: predict(image, 'penguins')
-predict_shadows = lambda image: predict(image, 'shadows')
